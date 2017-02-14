75b7276ce3b407dab0fe77452975f6d25f57efd1
劉哲宇 <j2325138@gmail.com>
2017-02-14 21:14:52 +0800

    test set accuracy

--- script.m
+++ script.m
@@ -16,11 +16,18 @@ y = loadMNISTLabels('data/train-labels.idx1-ubyte');
 X = X(1:10000, :);
 y = y(1:10000, :);
 m = size(X, 1);
-
 % Trasform Y from 0~9 to logical vector
 Y = zeros(size(y,1), num_labels);
 for i = 1:size(Y,1)
   Y(i, y(i)+1) = 1;
+end
+
+% Loading Test Data
+testX = loadMNISTImages('data/t10k-images.idx3-ubyte')';
+testy = loadMNISTLabels('data/t10k-labels.idx1-ubyte');
+testY = zeros(size(y,1), num_labels);
+for i = 1:size(testY,1)
+	testY(i, testy(i)+1) = 1;
 end
 
 if 0
@@ -65,13 +72,14 @@ for iter = 1:total_iteration
 	costs(iter) = cost;
 
 	[Theta1, Theta2, Theta3] = restoreTheta(nn_params, input_layer_size, hidden_layer1_size, hidden_layer2_size, num_labels);
-	pred = predict(Theta1, Theta2, Theta3, X);
-	fprintf('Accuracy: %f\n', mean(double(pred == (y+1))) * 100);
+	pred_train = predict(Theta1, Theta2, Theta3, X);
+	pred_test = predict(Theta1, Theta2, Theta3, testX);
+	accuracy_train = mean(double(pred_train == (y+1))) * 100;
+	accuracy_test = mean(double(pred_test == (testy+1))) * 100;
+	fprintf('accuracy(train, test): %f, %f\n', accuracy_train, accuracy_test);
 
 	fflush(stdout);
 end
-
-[Theta1, Theta2, Theta3] = restoreTheta(nn_params, input_layer_size, hidden_layer1_size, hidden_layer2_size, num_labels);
 
 save('paras.mat', 'input_layer_size', 'hidden_layer1_size', 'hidden_layer2_size', ...
 	 'num_labels', 'nn_params', 'costs', 'total_iteration', 'lambda', 'learning_rate');
@@ -107,6 +115,9 @@ pause;
 % fprintf('Program paused. Press enter to continue.\n');
 % pause;
 
-
-pred = predict(Theta1, Theta2, Theta3, X);
-fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == (y+1))) * 100);
+[Theta1, Theta2, Theta3] = restoreTheta(nn_params, input_layer_size, hidden_layer1_size, hidden_layer2_size, num_labels);
+pred_train = predict(Theta1, Theta2, Theta3, X);
+pred_test = predict(Theta1, Theta2, Theta3, testX);
+accuracy_train = mean(double(pred_train == (y+1))) * 100;
+accuracy_test = mean(double(pred_test == (testy+1))) * 100;
+fprintf('accuracy(train, test): %f, %f\n', accuracy_train, accuracy_test);
