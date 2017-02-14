d46c40ff7b91d48fa7f682a7902e9797b366f4a6
劉哲宇 <j2325138@gmail.com>
2017-02-14 14:26:30 +0800

    Yolo

--- nnCostFunction.m
+++ nnCostFunction.m
@@ -2,11 +2,11 @@ function [J grad] = nnCostFunction(nn_params, ...
                                    input_layer_size, ...
                                    hidden_layer_size, ...
                                    num_labels, ...
-                                   X, y, lambda)
+                                   X, Y, lambda)
 %NNCOSTFUNCTION Implements the neural network cost function for a two layer
 %neural network which performs classification
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
-%   X, y, lambda) computes the cost and gradient of the neural network. The
+%   X, Y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
 %   nn_params and need to be converted back into the weight matrices. 
 % 
@@ -22,20 +22,7 @@ Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
                  num_labels, (hidden_layer_size + 1));
 
-% Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
-J = 0;
-Theta1_grad = zeros(size(Theta1));
-Theta2_grad = zeros(size(Theta2));
-
-% Trasform Y from 1~10 to binary vector
-Y = zeros(size(y,1), num_labels);
-for i = 1:size(Y,1)
-	Y(i, y(i)) = 1;
-end
-
 
 % Forward prop
 A1 = [ones(size(X,1), 1), X];
@@ -72,9 +59,6 @@ Theta2_grad = delta_2 ./ m;
 Theta1_grad(:, 2:end) += lambda .* Theta1(:, 2:end) ./ m;
 Theta2_grad(:, 2:end) += lambda .* Theta2(:, 2:end) ./ m;
 
-% -------------------------------------------------------------
-
-% =========================================================================
 
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
---
+++ script.m
@@ -0,0 +1,93 @@
+%% Initialization
+clear ; close all; clc; warning off;
+
+%% Setup the parameters you will use for this exercise
+input_layer_size  = 28 * 28;  % 28x28 Input Images of Digits
+hidden_layer_size = 25;   % 25 hidden units
+num_labels = 10;          % 10 labels, from 0 to 9
+
+% Load Training Data
+fprintf('Loading and Visualizing Data ...\n')
+
+X = loadMNISTImages('data/train-images-idx3-ubyte')';
+y = loadMNISTLabels('data/train-labels.idx1-ubyte');
+m = size(X, 1);
+
+% Trasform Y from 0~9 to logical vector
+Y = zeros(size(y,1), num_labels);
+for i = 1:size(Y,1)
+  Y(i, y(i)+1) = 1;
+end
+
+% Randomly select 100 datas to display
+sel = randperm(size(X, 1));
+sel = sel(1:100);
+
+displayData(X(sel, :));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+
+fprintf('\nInitializing Neural Network Parameters ...\n')
+initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+
+% Unroll parameters
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+
+fprintf('\nChecking gradient and initial_cost.\n');
+% checkNNGradients;
+initial_cost = nnCostFunction(initial_nn_params, input_layer_size, hidden_layer_size, num_labels, X, Y, 0)
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+
+fprintf('\nnTraining Neural Network normal gradient decend... \n');
+total_iteration = 300;
+nn_params = initial_nn_params;
+learning_rate = 0.04;
+lambda = 1;
+for iter = 1:total_iteration
+  [cost, grad] = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, num_labels, X, Y, lambda);
+  fprintf('iteration: %-3d, cost: %f\n', iter, cost);
+  fflush(stdout);
+  nn_params -= learning_rate .* grad;
+end
+
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+
+% fprintf('\nTraining Neural Network with fmincg... \n')
+% options = optimset('MaxIter', 50);
+
+% lambda = 1;
+% costFunction = @(p) nnCostFunction(p, ...
+%                                    input_layer_size, ...
+%                                    hidden_layer_size, ...
+%                                    num_labels, X, y, lambda);
+% [nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+
+% Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+%                  hidden_layer_size, (input_layer_size + 1));
+
+% Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+%                  num_labels, (hidden_layer_size + 1));
+
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
+
+
+
+pred = predict(Theta1, Theta2, X);
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y+1)) * 100);
